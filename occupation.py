9bb9805f31605fc32d6f4ac8434f5743dce08cd1
Sasha Fomina <afomina@stuy.edu>
2017-09-26 14:06:20 -0400

    stuff

---
+++ occupation.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template
+
+my_app = Flask(__name__)
+
+
+def fileOpen(filename):
+    inStream = open(str(filename), "r")
+    Input = inStream.readlines()
+    inStream.close()
+    return Input
+
+fileInfo = fileOpen("occupations.csv")
+
+import random
+
+def tidify(jobsList):
+    counter = 0
+    for string in jobsList:
+        jobsList[counter] = string.strip("\r\n")
+        counter += 1
+    return jobsList
+
+fileInfo = tidify(fileInfo)               
+
+def listify(jobsList):
+    counter = 0
+    for string in jobsList:
+        if string[0] == '"':
+            jobsList[counter] = string.split(",")
+            while len(jobsList[counter]) != 2:
+                jobsList[counter][0] =  jobsList[counter][0] + "," + jobsList[counter][1]
+                jobsList[counter].pop(1)
+        else :
+            jobsList[counter] = string.split(",")
+        counter += 1
+    return jobsList
+
+fileInfo =  listify(fileInfo)
+                
+def dictify(listOfList):
+    d = {} #dictionary
+    for l in listOfList:
+        #print l[0]
+        d[l[0]] = l[1]
+    return d
+
+#print fileInfo
+fileDictionary = dictify(fileInfo)
+#print fileDictionary
+
+def weightedRandom(d):
+    randNum = (1.0 * random.randint(0, 998)) / 10
+    keys = d.keys()
+    for string in keys:
+        if string != "Job Class" and string != "Total":
+            randNum = randNum - float(d[string])
+        if randNum < 0:
+            return string
+
+
+@my_app.route('/occupations')
+    
+return render_template('occupations.html', k = fileDictionary.keys(), d = fileDictionary)
