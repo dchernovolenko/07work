ba4b782eb75dfbe25552e1f02fcbc6fac38cf5a4
Daniel Cher <dchernovolenko@stuy.edu>
2017-10-03 08:50:07 -0400

    commit

--- flaskapp.py
+++ flaskapp.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
-from flask import Flask, render_template
-from utils import occupation
-#import occupation, csv, random
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-        return "root"
+        return render_template('root.html')
 
-@app.route("/occupations")
-def main():
-    return render_template('occupations.html', listkeys = occupation.createDict().keys(), d = occupation.createDict(), randomJob = occupation.weightedRandom(occupation.createDict()))
+@app.route("/response", methods = ["GET", "POST"])
+def response():
+    return render_template("responsetemp.html", usr = request.args["name"], mtd = request.method)
 
 if __name__ == "__main__":
         app.debug = True
